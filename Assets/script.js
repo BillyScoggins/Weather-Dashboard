b1de13d1740ae07edf801ee2b88dd448f8ffb3d6
Billy Scoggins <118499026+BillyScoggins@users.noreply.github.com>
2023-04-04 14:53:52 -0500

    created current conditions in html. Created fetch response. Created read me file

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,4 @@
-
+var cityform = document.getElementById("citySelector");
 var apiKey = "6bc02efad81fd6e69c73b3f035eba587"
 
 var city = "dallas,tx"
@@ -15,6 +15,35 @@ fetch(coordinatesURL)
   .then(function (data) {
     console.log(data);
   });
-
   
+cityform.addEventListener("submit", function(event){
+  event.preventDefault();
+  console.log(event);
+})   
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=6bc02efad81fd6e69c73b3f035eba587&units=metric')
+  .then(response => response.json())
+  .then(data => console.log(data));
 
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=6bc02efad81fd6e69c73b3f035eba587&units=metric')
+  .then(response => response.json())
+  .then(data => {
+    const weatherData = {
+      temperature: data.main.temp,
+      description: data.weather[0].description,
+      icon: data.weather[0].icon
+    };
+    console.log(weatherData);
+  });
+
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=6bc02efad81fd6e69c73b3f035eba587&units=metric')
+  .then(response => response.json())
+  .then(data => {
+    const weatherData = {
+      temperature: data.main.temp,
+      description: data.weather[0].description,
+      icon: data.weather[0].icon
+    };
+    document.getElementById('temperature').textContent = weatherData.temperature;
+    document.getElementById('description').textContent = weatherData.description;
+    document.getElementById('icon').setAttribute('src', `https://openweathermap.org/img/w/${weatherData.icon}.png`);
+  });
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Weather-Dashboard
+
+## I wanted to create a weather application that would allow people to view current and future weather conditions in either their own, or other cities.
+
+My motivation for building this application was to allow people to know local current and future weather conditions, as well as weather conditions in other cities, which would be a benefit to people that are traveling.
+
+I built this project as a resource for people to use in preparation for either the day where they live, or to prepare for travel.
+
+This application helps people plan what to pack for the weather conditions if theyre visiting another city.
+
+I learned a lot about API's and how they're applied, as well as they're function and necessity in today's programming world.
