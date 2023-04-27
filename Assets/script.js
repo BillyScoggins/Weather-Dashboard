599fba8506d089a94a9cebc1da554b49e23fef3d
Billy Scoggins <118499026+BillyScoggins@users.noreply.github.com>
2023-04-27 14:02:49 -0500

    updated five day forecast

--- Assets/script.js
+++ Assets/script.js
@@ -4,6 +4,7 @@ var temp = document.getElementById("temp");
 var humidity = document.getElementById("humidity");
 var windSpeed = document.getElementById("wind-speed");
 var kelvin = 300;
+var forecastDiv = document.getElementById("forecast");
 
 var apiKey = "6bc02efad81fd6e69c73b3f035eba587"
 
@@ -23,6 +24,7 @@ function getCurrWeather(city) {
   var coordinatesURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
 
   fetch(coordinatesURL)
+
 
     .then(function (response) {
       console.log("RESPONSE: ", response);
@@ -47,21 +49,48 @@ function getCurrWeather(city) {
         })
         .then(function (forecastData) {
           console.log(forecastData)
-          
-          for (let i = 0; i < forecastData.list.length; i++) {
-            
-          
-            temperature.textContent = forecastData.list[i].main.temp;
-            humid.textContent = forecastData.list[i].main.humidity;
-            weatherConditions.textContent = forecastData.list[i].weather[0].description;
-            windspeed.textContent = forecastData.list[i].wind.speed;
-        }})
+
+          for (let i = 0; i < forecastData.list.length; i = i + 8) {
+            const currentData = forecastData.list[i];
+            const row = document.createElement("tr");
+
+            const dateCell = document.createElement("td");
+            dateCell.textContent = currentData.dt_txt;
+            row.appendChild(dateCell);
+            forecastDiv.appendChild(row);
+
+            const tempCell = document.createElement("td");
+            tempCell.textContent = currentData.main.temp;
+            row.appendChild(tempCell);
+            forecastDiv.appendChild(row);
+
+            const condCell = document.createElement("td");
+            condCell.textContent = currentData.weather[0].description;
+            row.appendChild(condCell);
+            forecastDiv.appendChild(row);
+
+            const windSpeedCell = document.createElement("td");
+            windSpeedCell.textContent = currentData.wind.speed;
+            row.appendChild(windSpeedCell);
+            forecastDiv.appendChild(row);
+
+            const humidCell = document.createElement("td");
+            humidCell.textContent = currentData.main.humidity;
+            row.appendChild(humidCell);
+            forecastDiv.appendChild(row);
+
+             
+          }
+        })
 
 
     });
-}
 
-//const weatherForecast = ["Date", "Temp", "Humidity", "Conditions", "Wind-speed"];
+    
+  }
+  
+
+
 
 
 
